cd92cb73ccd73fd0231ee09ce9469d81328a9d4d
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-04-26 20:40:01 +0900

    トップページをメモ作成画面に変更

--- simplenote/app/Http/Controllers/HomeController.php
+++ simplenote/app/Http/Controllers/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
     {
         $user = \Auth::user();
         $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
-        return view('home', compact('user', 'memos'));
+        return view('create', compact('user', 'memos'));
     }
 
     public function create()
